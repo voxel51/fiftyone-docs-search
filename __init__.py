e43d09793b8c8c8cfc145891cec8e932ed326a35
Ritchie Martori <ritchie@voxel51.com>
2023-04-17 15:01:00 -0700

    add operator impl

---
+++ __init__.py
@@ -0,0 +1,44 @@
+import fiftyone.operators as foo
+import fiftyone.operators.types as types
+
+from fiftyone.docs_search.query_index import query_index
+
+
+class SearchDocs(foo.DynamicOperator):
+  def __init__(self):
+    super().__init__(
+      "search-docs",
+      "Searches the FiftyOne docs",
+    )
+  
+  def resolve_input(self, ctx):
+    inputs = types.Object()
+    inputs.str("query", label="Query", required=True)
+    return types.Property(inputs)
+
+  def execute(self, ctx):
+    results = query_index(
+        ctx.params.get("query")
+    )
+    links = []
+    for url, text, score in results:
+      links.append({
+        "href": url,
+        "label": text
+      })
+    return {"links": links}
+
+  def resolve_output(self, ctx):
+    outputs = types.Object()
+    outputs.define_property("links", types.List(types.Link()))
+    return types.Property(outputs)
+
+
+op = None
+
+def register():
+  op = SearchDocs()
+  foo.register_operator(op)
+
+def unregister():
+  foo.unregister_operator(op)
